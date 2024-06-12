a69c44e58f6fe8f23279e18748006a556903b649
Daniil-KDK <kopach.dk@mail.ru>
2024-06-12 16:42:19 +0300

    task2 and task3

---
+++ HW1/task2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//*вывод текста на экран лесенкой*
+
+int main(int argc, char **argv)
+{
+   printf("   Let's\n");
+   printf("        go\n");
+   printf("          to walk\n");
+
+   return 0;
+}
